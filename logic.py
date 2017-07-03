67026a6a82dc9e5da6b66f9677d29f06086e78ec
Tundish <tundish@thuswise.org>
2017-07-03 12:37:30 +0100

    Added first few spots.

---
+++ logic.py
@@ -0,0 +1,10 @@
+import enum
+
+from turberfield.dialogue.types import EnumFactory
+from turberfield.utils.assembly import Assembly
+
+class Spot(EnumFactory, enum.Enum):
+    w12_ducane_prison_approach = "gcpv4d2dm6v2"
+    w12_goldhawk_cafe_approach = "gcpufzg2512x"
+    w12_goldhawk_tavern_approach = "gcpufzbd8x5d"
+    w12_latimer_arches_approach = "gcpv4cxb3dh4"
