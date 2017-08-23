fb6b7cecc55f9f460cdacb8a72aec5ba06fa1b77
Tundish <tundish@thuswise.org>
2017-08-23 17:47:54 +0100

    Separated performer module.

--- bluemonday78/test/test_performer.py
+++ bluemonday78/test/test_performer.py
@@ -19,61 +19,11 @@
 import copy
 import unittest
 
-from turberfield.dialogue.model import Model
-from turberfield.dialogue.model import SceneScript
 from turberfield.utils.misc import group_by_type
 
 from bluemonday78.logic import ensemble, plotlines, schedule
+from bluemonday78.performer import Performer
 
-
-class Performer:
-
-    @staticmethod
-    def next(folders, ensemble, strict=True, roles=1):
-        for folder in folders:
-            scripts = SceneScript.scripts(**folder._asdict())
-            for script in scripts:
-                with script as dialogue:
-                    selection = dialogue.select(ensemble, roles=roles)
-                    if all(selection.values()):
-                        return (script, selection)
-                    elif not strict and any(selection.values()):
-                        return (script, selection)
-        else:
-            return None
-
-    @property
-    def stopped(self):
-        return not bool(self.next(self.folders, self.ensemble))
-
-    def init(self, folders, ensemble):
-        self.folders = folders
-        self.ensemble = ensemble
-
-    def __init__(self, folders, ensemble):
-        self.shots = []
-        self.init(folders, ensemble)
-
-    def react(self, obj):
-        if isinstance(obj, Model.Property):
-            if obj.object is not None:
-                setattr(obj.object, obj.attr, obj.val)
-        return obj
-
-    def run(self, react=True):
-        try:
-            script, selection = self.next(self.folders, self.ensemble)
-        except TypeError:
-            raise GeneratorExit
-        with script as dialogue:
-            model = dialogue.cast(selection).run()
-            for shot, item in model:
-                yield shot
-                yield item
-                if not self.shots or self.shots[-1][:2] != shot[:2]:
-                        self.shots.append(shot._replace(items=script.fP))
-                if react:
-                    self.react(item)
 
 class TestPerformer(unittest.TestCase):
 
@@ -99,5 +49,3 @@ class TestPerformer(unittest.TestCase):
         performer = Performer(self.schedule, self.ensemble)
         while not performer.stopped:
             list(performer.run())
-            print(*performer.shots, sep="\n")
-            print()
