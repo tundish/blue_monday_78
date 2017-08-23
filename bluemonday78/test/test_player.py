1363a43ecb22b6c762213f4c442f10e3e1b1a6cd
Tundish <tundish@thuswise.org>
2017-08-23 14:51:12 +0100

    Added new test module.

---
+++ bluemonday78/test/test_player.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# encoding: UTF-8
+
+# This file is part of Addison Arches.
+#
+# Addison Arches is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Addison Arches is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Addison Arches.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+from turberfield.utils.misc import group_by_type
+
+
+class Player:
+
+    @staticmethod
+    def stopped(folders, ensemble):
+        return None
+
+class TestPlayer(unittest.TestCase):
+
+    def test_stopped(self):
+        self.fail()
