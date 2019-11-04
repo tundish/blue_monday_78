13e88b7f5f30453e2957862b148e513e915d7427
Tundish <tundish@gigeconomy.org.uk>
2019-11-04 18:48:28 +0000

    Quick publisher hack of uuid location.

--- bluemonday78/utils/publisher.py
+++ bluemonday78/utils/publisher.py
@@ -16,4 +16,46 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Addison Arches.  If not, see <http://www.gnu.org/licenses/>.
 
+import argparse
+import pathlib
+import sys
+import uuid
 
+__doc__ = """
+Generate Python code to describe SceneScript folders from a directory
+structure.
+
+"""
+
+def find_locations(path):
+    for id_location in path.glob("**/uuid.hex"):
+        try:
+            uid = uuid.UUID(hex=id_location.read_text().strip())
+        except (ValueError, OSError):
+            print("Bad uuid at '", id_location, "'.", sep="", file=sys.stderr)
+            continue
+        else:
+            yield uid
+
+def main(args):
+    for path in args.paths:
+        print(*list(find_locations(path)), sep="\n")
+    return 0
+
+def parser(description=__doc__):
+    rv = argparse.ArgumentParser(
+        description,
+        fromfile_prefix_chars="@"
+    )
+    rv.add_argument(
+        "paths", nargs="*", type=pathlib.Path,
+        help="supply a list of directory paths"
+    )
+    return rv
+
+
+if __name__ == "__main__":
+    p = parser()
+    args = p.parse_args()
+    rv = main(args)
+    sys.exit(rv)
